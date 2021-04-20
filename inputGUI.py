29cea9b049144fad61eb7cdef320630c82f9a673
ScreamingOranges <daniel.ksel@gmail.com>
2021-04-19 22:53:16 -0400

    GUI for key input

---
+++ inputGUI.py
@@ -0,0 +1,20 @@
+import sys
+import PyQt5
+import PyQt5.QtWidgets
+
+class inputGUI(PyQt5.QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+    def getText(self):
+        pusherKey, okPressed = PyQt5.QtWidgets.QInputDialog.getText(self, "iCueConnect","Enter Pusher Key:", PyQt5.QtWidgets.QLineEdit.Normal, "")
+        if okPressed and pusherKey != '':
+            return pusherKey
+        sys.exit()
+
+"""
+if __name__ == '__main__':
+    app = PyQt5.QtWidgets.QApplication(sys.argv)
+    app.setQuitOnLastWindowClosed(False)
+    pusherKey = inputGUI().getText()
+""" 
